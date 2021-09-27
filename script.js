73b6e58f7c031dd94d99a539745074de027bfa84
Allan Pennycuff <allanp1238@gmail.com>
2021-09-27 10:47:23 -0500

    Corrected script.js and completed the bonus.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let randomNum = Math.floor(Math.random() * 6)
+let randomNum = Math.floor(Math.random() * 5 + 1);
+console.log(randomNum);
 if (randomNum >= 4) {
   console.log("Greater than or equal to 4");
-} else if (randomNum = 2 || 3) {
+} else if (randomNum == 2 || randomNum == 3) {
   console.log("Equal to 2 or 3");
 } else {
   console.log("Equal to 1");
@@ -12,6 +13,26 @@ if (randomNum != 3) {
 if (randomNum != 3 && randomNum != 5) {
   console.log("NOT equal to 3 AND not equal to 5");
 }
-if (randomNum = 2 || 4) {
+if (randomNum === 2 || randomNum === 4) {
   "Equal to 2 OR equal to 4"
+}
+
+// BONUS
+random >=4 ? console.log("Greater than or equal to 4") : console.log("Less than 4");
+switch(randomNum) {
+  case 1:
+    console.log("One");
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  case 3:
+    console.log("Three");
+    break;
+  case 4:
+    console.log("Four");
+    break;
+  case 5:
+    console.log("Five");
+    break;
 }
